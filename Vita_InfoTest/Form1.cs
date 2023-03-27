064b66f588e22fc161b3dc336a47a8c9c4a5b864
vita.19333 <vita.19333@LABI1-6PC04>
2023-03-27 10:55:44 +0200

    VitaT_Task5

--- Vita_InfoTest/Form1.cs
+++ Vita_InfoTest/Form1.cs
@@ -16,5 +16,60 @@ namespace Vita_InfoTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        List<VitaT_Candidato> l1 = new List<VitaT_Candidato>();
+
+        public void Add(VitaT_Candidato c)
+        {
+            l1.Add(c);
+        }
+
+        public string[] Stampa()
+        {
+            List<string> lista = new List<string>();
+            foreach (VitaT_Candidato c in l1)
+            {
+                lista.Add(c.ToString());
+            }
+            return lista.ToArray();
+        }
+
+        public void Modifica(string nome, int nmatricola)
+        {
+            for (int i = 0; i < l1.Count; i++)
+            {
+                if (l1[i].Matricola == nmatricola)
+                {
+                    l1[i].Nome = nome;
+                }
+            }
+        }
+
+
+        public void Elimina(int nmatricola)
+        {
+            for (int i = 0; i < l1.Count; i++)
+            {
+                if (l1[i].Matricola == nmatricola)
+                {
+                    l1[i] = null;
+                }
+            }
+        }
+
+
+        public string[] VisualizzaIdonei()
+        {
+
+            List<string> l2 = new List<string>();
+
+        }
+
     }
 }
